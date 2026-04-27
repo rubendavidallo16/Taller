f473ba40b20a101ea447b4ca5476b92c482e815c
rubendavidallo16 <rubendavidallo@ufps.edu.co>
2026-04-27 00:03:18 -0500

    Se corrige error de sintaxis en url de supabase

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,7 +5,7 @@ window.CONFIG = {
   ROLE_KEY: 'paddock_role',
   USERNAME_KEY: 'paddock_username',
   RECAPTCHA_SITE_KEY: '6LcqQswsAAAAAEmKF2bI483dLGnldhe1P1RWgLi-',
-  SUPABASE_URL: 'https://afsvnqxcglkhaglvionm.supabase.co',
+  SUPABASE_URL: 'https://afsvnqxclgkhaglviomn.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmc3ZucXhjbGdraGFnbHZpb21uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyNTA4MjAsImV4cCI6MjA5MjgyNjgyMH0.zSi5G8PGBQM38ADUkFr_vEYFSvjXndznIC4M8QiQBRA'
   // El Anon Key es público y seguro de poner en el frontend.
 };
