71e43e51ef67c85b9506d45c401b08c8bb538e33
rubendavidallo16 <rubendavidallo@ufps.edu.co>
2026-05-07 22:12:02 -0500

    Se cambia el anon public y URL de supabase

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,7 +5,7 @@ window.CONFIG = {
   ROLE_KEY: 'paddock_role',
   USERNAME_KEY: 'paddock_username',
   RECAPTCHA_SITE_KEY: '6LcqQswsAAAAAEmKF2bI483dLGnldhe1P1RWgLi-',
-  SUPABASE_URL: 'https://hxyqiogduegbdlzgdylc.supabase.co/rest/v1/',
+  SUPABASE_URL: 'https://hxyqiogduegbdlzgdylc.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4eXFpb2dkdWVnYmRsemdkeWxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMDU3MjIsImV4cCI6MjA5MzY4MTcyMn0.SBYeG0nqGyuIbVTIjA2QTw9tmtXikPSUgm8vLxFdvwY'
   // El Anon Key es público y seguro de poner en el frontend.
 };
